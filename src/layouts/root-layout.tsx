c311017fac29a48dc3d07e7603a7093688af68be
tilav-web <shavqiddintilovov0@gmail.com>
2025-10-16 10:34:08 +0500

    .

--- src/layouts/root-layout.tsx
+++ src/layouts/root-layout.tsx
@@ -1,13 +1,18 @@
 import { userService } from "@/services/user.service";
 import { useUserStore } from "@/stores/user.store";
+import { handleStorage } from "@/utils/handle-storage";
 import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import { Outlet } from "react-router-dom";
 
 export default function RootLayout() {
   const { setUser, user, logout } = useUserStore();
-
+  const { i18n } = useTranslation();
   useEffect(() => {
     (async () => {
+      i18n.changeLanguage(
+        user?.lan ? user?.lan : handleStorage({ key: "lan" }) ?? "uz"
+      );
       try {
         if (user || user === null) return;
         const data = await userService.findMe();
